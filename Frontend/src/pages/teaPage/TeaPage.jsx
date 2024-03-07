e7063a3fc4692f56431abefd3aabb5990fc2a2c5
YZeaFons <yeisonyamidzea@gmail.com>
2024-03-07 13:04:16 -0500

    temporary changes

---
+++ Frontend/src/pages/teaPage/TeaPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const TeaPage = () => {
+
+    const [drinks, setDrinks] = useState([])
+
+
+    const bringDrinks = () => {
+
+    }
+
+
+
+    return (
+        <div>TeaPage</div>
+    )
+}
+
+export default TeaPage
