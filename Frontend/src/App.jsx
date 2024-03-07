1cf341c45ffa55ecaa54965e4cfb9e14153d62b6
YZeaFons <yeisonyamidzea@gmail.com>
2024-03-07 12:32:36 -0500

    front structure changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
-import NavBar from './components/navBar/NavBar'
 import Admin from './pages/adminWindow/AdminWindow'
 import MainFoodMenu from './pages/mainFoodMenu/MainFoodMenu'
-import Footer from './components/footer/Footer'
 
 
 
@@ -13,13 +11,11 @@ function App() {
 
   return (
     <>
-      {<NavBar />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/admin' element={<Admin />} />
         <Route path='/foodMenu' element={<MainFoodMenu />} />
       </Routes>
-      {<Footer />}
     </>
   )
 }
