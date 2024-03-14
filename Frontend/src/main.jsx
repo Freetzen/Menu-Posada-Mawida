75a017219c466c9c605880e16ef74369c154f37d
agusasaad <agusasaad1099@hotmail.com>
2024-03-14 10:11:57 -0300

    home desing 70%

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
+);
